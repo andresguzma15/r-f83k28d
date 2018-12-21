6634dc8e471648fe88c0cf2e9fe49c0d556d587e
andresguzma15 <andresguzma15@gmail.com>
2018-12-20 21:11:56 -0500

    hola

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
 class App extends Component {
+	state = { counter: 0 }
+
+	incrementCount = () => {
+		this.setState( { counter: this.state.counter +1 });
+	}
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">
+        	{this.state.counter}
+        </span>
+        <button className="btn" onClick={this.incrementCount}>Increment</button>
       </div>
     );
   }
 }
 
 export default App;
+
